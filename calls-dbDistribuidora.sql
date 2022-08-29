09d77ab9e4c5ce7acdf382895cda177bdb0d6671
Levi Bergamascki <82347503+levibergamascki@users.noreply.github.com>
2022-08-28 22:25:43 -0300

    Add files via upload

---
+++ calls-dbDistribuidora.sql
@@ -0,0 +1,74 @@
+call spInsertFornecedor(1245678937123, "Revenda Chico Loco", 11934567897);
+call spInsertFornecedor(1345678937123, "José Faz Tudo S/A", 11934567898);
+call spInsertFornecedor(1445678937123, "Vadalto Entregas", 11934567899);
+call spInsertFornecedor(1545678937123, "Astrogildo das Estrelas", 11934567800);
+call spInsertFornecedor(1645678937123, "Amoroso e Doce", 11934567801);
+call spInsertFornecedor(1745678937123, "Marcelo Dedal", 11934567802);
+call spInsertFornecedor(1845678937123, "Franciscano Cachaça", 11934567803);
+call spInsertFornecedor(1945678937123, "Joãozinho Chupeta", 11934567804);
+
+call spInsertCidade(1, "Rio de Janeiro");
+call spInsertCidade(2, "São Carlos");
+call spInsertCidade(3, "Campinas");
+call spInsertCidade(4, "Franco da Rocha");
+call spInsertCidade(5, "Osasco");
+call spInsertCidade(6, "Pirituba");
+call spInsertCidade(7, "Lapa");
+call spInsertCidade(8, "Ponta Grossa");
+call spInsertCidade(9, "São Paulo");
+call spInsertCidade(10, "Barra Mansa");
+
+call spInsertUF(1, "SP");
+call spInsertUF(2, "RJ");
+call spInsertUF(3, "RS");
+
+call spInsertBairro(1, "Aclimação");
+call spInsertBairro(2, "Capão Redondo");
+call spInsertBairro(4, "Liberdade");
+call spInsertBairro(5, "Lapa");
+call spInsertBairro(6, "Penha");
+call spInsertBairro(7, "Consolação");
+
+call spInsertProduto(12345678910111,'Rei de Papel Mache',54.61,120);
+call spInsertProduto(12345678910112,'Bolinha de Sabão',100.45,120);
+call spInsertProduto(12345678910113,'Barro Bate Bate',44.00,120);
+call spInsertProduto(12345678910114,'Bola Furada',10.00,120);
+call spInsertProduto(12345678910115,'Maçã Laranja',99.44,120);
+call spInsertProduto(12345678910116,'Boneco do Hitler',124.00,200);
+call spInsertProduto(12345678910117,'Farinha de Surui',50.00,200);
+call spInsertProduto(12345678910118,'Zelador de Cemitério',24.50,100);
+
+call spInsertEndereco(12345050, "Rua da Federal", "Lapa", "São Paulo", "SP");
+call spInsertEndereco(12345051, "Av Brasil", "Lapa", "Campinas", "SP");
+call spInsertEndereco(12345052, "Rua Liberdade", "Consolação", "São Paulo", "SP");
+call spInsertEndereco(12345053, "Ab Paulista", "Penha", "Rio de Janeiro", "RJ");
+call spInsertEndereco(12345054, "Rua Ximbú", "Penha", "Rio de Janeiro", "RJ");
+call spInsertEndereco(12345055, "Rua Piu X1", "Penha", "Campinas", "SP");
+call spInsertEndereco(12345056, "Rua chocolate", "Aclimação", "Barra Mansa", "RJ");
+call spInsertEndereco(12345057, "Rua Pão na Chapa", "Barra Funda", "Ponta Grossa", "RS");
+call spInsertEndereco(12345050, "Rua da Federal", "Lapa", "São Paulo", "SP");
+
+call spInsertCliente('Pimpão', 325, null, 12345051, 12345678911, 12345678, 0, '2000-12-10', 'Av. Brasil', 'Lapa', 'Campinas', 'SP');
+call spInsertCliente('Disney Chaplin', 89, 'Ap. 12', 12345053, 12345678912, 12345679, 0, '2001-11-21', 'Av. Paulista', 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertCliente('Marciano', 744, null, 12345054, 12345678913, 12345680, 0, '2001-06-01', 'Rua Ximbú', 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertCliente('Lança Perfume', 128, null, 12345059, 12345678914, 12345681, 'X', '2004-04-05', 'Rua Veia', 'Jardim Santa Isabel', 'Cuiabá', 'MT');
+call spInsertCliente('Remédio Amargo', 2485, null, 12345058, 12345678915, 12345682, 0, '2002-07-15', 'Av. Nova', 'Jardim Santa Isabel', 'Cuiabá', 'MT');
+
+call spInsertClientePJ('Paganada', 12345678912345, 98765432198, 12345051, 'Av. Brasil', 159, null, 'Lapa', 'Campinas', 'SP');
+call spInsertClientePJ('Caloteando', 12345678912346, 98765432199, 12345053, 'Av. Paulista', 69, null, 'Penha', 'Rio de Janeiro', 'RJ');
+call spInsertClientePJ('Semgrana', 12345678912347, 98765432100, 12345060, 'Rua dos Amores', 189, null, 'Sei Lá', 'Recife', 'PE');
+call spInsertClientePJ('Cemreais', 12345678912348, 98765432101, 12345060, 'Rua dos Amores', 5024, 'Sala 23', 'Sei Lá', 'Recife', 'PE');
+call spInsertClientePJ('Durango', 12345678912349, 98765432102, 12345060, 'Rua dos Amores', 1254, null, 'Sei Lá', 'Recife', 'PE');
+
+call spInsertCompra(8459, 'Amoroso e Doce', '2018-05-01', 12345678910111, 22.22, 200, 700, 21944.00);
+call spInsertCompra(2482, 'Revenda Chico Loco', '2020-04-22', 12345678910112, 40.50, 180, 180, 7290.00);
+call spInsertCompra(21563, 'Marcelo Dedal', '2020-07-12', 12345678910113, 3.00, 300, 300, 900.00);
+call spInsertCompra(8459, 'Amoroso e Doce', '2020-12-04', 12345678910114, 35.00, 500, 700, 21944.00);
+call spInsertCompra(156354, 'Revenda Chico Loco', '2021-11-23', 12345678910115, 54.00, 350, 350, 18900.00);
+
+call spInsertVenda(1,"Pimpão","22-08-2022",12345678910111,54.61,1,54.61,null);
+call spInsertVenda(2,"Lança Perfume","22-08-2022",12345678910112,54.61,1,54.61,null);
+call spInsertVenda(3,"Pimpão","22-08-2022",12345678910113,100.45,2,200.90,null);
+
+call spInsertNotaFiscal(359,"Pimpão","22-08-2022");
+call spInsertNotaFiscal(360,"Lança Perfume","22-08-2022");
